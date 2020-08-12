d137b638cc58e86292de50d3d7e2caa2a887620b
ekram49 <ekramullahzaki@gmail.com>
2020-08-12 04:33:24 -0400

    day 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,24 @@
 
+from util import Queue
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    queue = Queue()
+    current_node = starting_node
+    cache = {}
+    for x in ancestors:
+        if x[1] not in cache:
+            cache[x[1]] = set()
+        cache[x[1]].add(x[0])
+
+    if starting_node in cache:
+        queue.enqueue(cache[current_node])
+    else:
+        return -1
+
+    while True:
+        relations = queue.dequeue()
+        current_node = min(relations)
+        if current_node not in cache:
+            return current_node
+        else:
+            queue.enqueue(cache[current_node])
